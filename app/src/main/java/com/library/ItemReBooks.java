3b7379f02db6dd90c35cc21dad64b414937bc562
AhmedRammadan <43143580+AhmedRammadan@users.noreply.github.com>
2018-10-27 02:03:44 +0200

    version 2

--- app/src/main/java/com/library/ItemReBooks.java
+++ app/src/main/java/com/library/ItemReBooks.java
@@ -1,25 +1,117 @@
 package com.library;
 
 public class ItemReBooks {
-    private int image ;
-    private String title,size;
+    private String id, title, iamge, desc_book, desc_writer, link, namewriter,size,page,category;
+    private int counter_download , view;
 
-    public ItemReBooks(int image, String title, String size) {
-        this.image = image;
-        this.title = title;
-        this.size = size;
+    public String getCategory() {
+        return category;
     }
 
-    public int getImage() {
-        return image;
+    public void setCategory(String category) {
+        this.category = category;
     }
 
-    public String getTitle() {
-        return title;
+    public int getView() {
+        return view;
+    }
+
+    public void setView(int view) {
+        this.view = view;
+    }
+
+    public int getCounter_download() {
+        return counter_download;
+    }
+
+    public void setCounter_download(int counter_download) {
+        this.counter_download = counter_download;
+    }
+
+    public String getnamewriter() {
+        return namewriter;
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+
+    public ItemReBooks(String name) {
     }
 
     public String getSize() {
         return size;
     }
 
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public ItemReBooks (String id, String title,String category,int view, int counter_download,String size , String page, String iamge, String desc_book, String desc_writer, String link, String namewriter) {
+        this.id = id;
+        this.title = title;
+        this.iamge = iamge;
+        this.desc_book = desc_book;
+        this.desc_writer = desc_writer;
+        this.link = link;
+        this.namewriter = namewriter;
+        this.size = size;
+        this.page = page;
+        this.category = category;
+        this.counter_download = counter_download;
+        this.view = view;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIamge() {
+        return iamge;
+    }
+
+    public void setIamge(String iamge) {
+        this.iamge = iamge;
+    }
+
+    public String getDesc_book() {
+        return desc_book;
+    }
+
+    public void setDesc_book(String desc_book) {
+        this.desc_book = desc_book;
+    }
+
+    public String getDesc_writer() {
+        return desc_writer;
+    }
+
+    public void setDesc_writer(String desc_writer) {
+        this.desc_writer = desc_writer;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
 }
+
